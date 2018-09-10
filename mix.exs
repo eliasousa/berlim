0a7bdcedc5495fe9960896d633f09d33f048fe7f
Elias Sousa <elias@jaya.tech>
2018-09-09 21:04:09 -0300

    Add tool-versions and overcommit.yml

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Berlim.Mixfile do
     [
       app: :berlim,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.7.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,

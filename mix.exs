88877de94b12b0321d6608a20a55321fd88a7f14
danpassoss <dann.passos@hotmail.com>
2019-01-21 10:43:47 -0300

    Upgrade elixir version to 1.8.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Berlim.MixProject do
     [
       app: :berlim,
       version: "0.0.1",
-      elixir: "~> 1.7.3",
+      elixir: "~> 1.8.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,

b2bd0b908c68edd79a9e84961d274282b68a3d6a
Elias Sousa <humb.eliasj@gmail.com>
2019-02-24 16:08:17 -0300

    Add users sign controller

---
+++ lib/berlim_web/controllers/user_controller.ex
@@ -0,0 +1,18 @@
+defmodule BerlimWeb.UserController do
+  use BerlimWeb, :controller
+
+  alias Berlim.Accounts
+  alias BerlimWeb.UserView
+
+  action_fallback(BerlimWeb.FallbackController)
+
+  def create(conn, %{"email" => email, "password" => password}) do
+    case Accounts.token_sign_in(email, password) do
+      {:ok, token, _claims} ->
+        conn |> render(UserView, "jwt.json", jwt: token)
+
+      _ ->
+        {:error, :unauthorized}
+    end
+  end
+end
--- lib/berlim_web/router.ex
+++ lib/berlim_web/router.ex
@@ -1,12 +1,28 @@
 defmodule BerlimWeb.Router do
   use BerlimWeb, :router
 
+  alias BerlimWeb.Plugs
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
+  pipeline :jwt_authenticated do
+    plug Plugs.AuthPipeline
+  end
+
+  pipeline :ensure_admin do
+    plug Plugs.RequireAdminAuth
+  end
+
   scope "/api", BerlimWeb do
     pipe_through :api
+
+    resources("/users", UserController, only: [:create])
+  end
+
+  scope "/api", BerlimWeb do
+    pipe_through [:api, :jwt_authenticated, :ensure_admin]
 
     resources("/admins", AdminController, except: [:new, :edit])
     resources("/taxis", TaxiController, except: [:new, :edit, :delete])

b0e76c0495ebe871a01efcef1d8e4b83be93072c
Elias Sousa <humb.eliasj@gmail.com>
2019-02-27 12:07:58 -0300

    Fix tests (#31)

--- test/berlim_web/views/admin_view_test.exs
+++ test/berlim_web/views/admin_view_test.exs
@@ -17,13 +17,13 @@ defmodule BerlimWeb.AdminViewTest do
 
   describe "index.json/2" do
     test "returns admins", %{admin: admin} do
-      assert %{data: [admin]} = AdminView.render("index.json", %{admins: [admin]})
+      assert AdminView.render("index.json", %{admins: [admin]}) == %{data: [admin]}
     end
   end
 
   describe "show.json/2" do
     test "returns admin", %{admin: admin} do
-      assert %{data: admin} = AdminView.render("show.json", %{admin: admin})
+      assert AdminView.render("show.json", %{admin: admin}) == %{data: admin}
     end
   end
 

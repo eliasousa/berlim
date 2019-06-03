dae19f322a2c017b39b6875678661f86386f0324
Elias Sousa <humb.eliasj@gmail.com>
2019-06-03 09:53:21 -0300

    Block inactive users login and enable taxi login by smtt (#45)
    * Block login for inactive users
    
    * Enable taxi login by smtt
    
    * Fix relationship

--- test/berlim/accounts/accounts_test.exs
+++ test/berlim/accounts/accounts_test.exs
@@ -17,10 +17,20 @@ defmodule Berlim.AccountsTest do
     assert claims["type"] == "Admin"
   end
 
-  test "token_sign_in/2 with valid taxi credentials" do
+  test "token_sign_in/2 with valid taxi credentials using email" do
     taxi = insert(:taxi, %{email: "john@email.com", encrypted_password: hash_pwd_salt("123456")})
 
     {:ok, token, claims} = Accounts.token_sign_in("john@email.com", "123456")
+
+    assert is_binary(token)
+    assert claims["sub"] == Integer.to_string(taxi.id)
+    assert claims["type"] == "Taxi"
+  end
+
+  test "token_sign_in/2 with valid taxi credentials using smtt" do
+    taxi = insert(:taxi, %{smtt: "7849", encrypted_password: hash_pwd_salt("123456")})
+
+    {:ok, token, claims} = Accounts.token_sign_in("7849", "123456")
 
     assert is_binary(token)
     assert claims["sub"] == Integer.to_string(taxi.id)
@@ -42,5 +52,35 @@ defmodule Berlim.AccountsTest do
     insert(:admin, %{email: "john@email.com", encrypted_password: "123456"})
 
     assert {:error, :unauthorized} = Accounts.token_sign_in("john@email.com", "12345678")
+  end
+
+  test "token_sign_in/2 when admin is inactive" do
+    insert(:admin, %{
+      email: "john@email.com",
+      encrypted_password: hash_pwd_salt("123456"),
+      active: false
+    })
+
+    assert {:error, :unauthorized} = Accounts.token_sign_in("john@email.com", "123456")
+  end
+
+  test "token_sign_in/2 when taxi is inactive" do
+    insert(:taxi, %{
+      email: "john@email.com",
+      encrypted_password: hash_pwd_salt("123456"),
+      active: false
+    })
+
+    assert {:error, :unauthorized} = Accounts.token_sign_in("john@email.com", "123456")
+  end
+
+  test "token_sign_in/2 when company is inactive" do
+    insert(:company, %{
+      email: "john@email.com",
+      encrypted_password: hash_pwd_salt("123456"),
+      active: false
+    })
+
+    assert {:error, :unauthorized} = Accounts.token_sign_in("john@email.com", "123456")
   end
 end
